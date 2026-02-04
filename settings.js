4e7edda0188094c66ad526452ff7179f6d1e1912
JarrStore <163243032+JarrStore@users.noreply.github.com>
2026-02-04 08:24:02 +0700

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,14 @@
+module.exports = {
+    storage: "mongodb", // "json" | "mysql" | "mongodb"
+
+    mysql: {
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "akun_app"
+    },
+
+    mongodb: {
+        uri: "mongodb+srv://hanyabutuhakses_db_user:2RjkYnI6xRxpMpFJ@cluster0.pcppdz1.mongodb.net/?appName=Cluster0"
+    }
+};
